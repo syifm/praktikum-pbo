2dd3c176acc7b37104c89a7dd844595145760d87
Asy'syifa Shabrina Munir <131370960+syifm@users.noreply.github.com>
2024-02-20 14:50:01 +0700

    Titik.java

---
+++ Praktikum 1/Titik.java
@@ -0,0 +1,37 @@
+public class Titik{
+    double absis;
+    double ordinat;
+    static double counterTitik;
+
+    Titik(){
+        absis = 0;
+        ordinat = 0;
+        counterTitik++;
+    }
+
+    Titik(double a, double o){
+        absis = a;
+        ordinat = o;
+        counterTitik++;
+    }
+
+    void setAbsis(double a){
+        absis = a;
+    }
+
+    void setOrdinat(double o){
+        ordinat = o;
+    }
+
+    double getAbsis(){
+        return absis;
+    }
+
+    double getOrdinat(){
+        return ordinat;
+    }
+
+    static double getCounterTitik(){
+        return counterTitik;
+    }
+}
