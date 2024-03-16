e564b1a3b1587416633439a3c9a951a8f07fb781
Asy'syifa Shabrina Munir <131370960+syifm@users.noreply.github.com>
2024-03-16 14:27:04 +0700

    Poligon.java

---
+++ Praktikum 4/Poligon.java
@@ -0,0 +1,16 @@
+/*
+ * Nama      : Asy'syifa Shabrina Munir
+ * NIM       : 24060122130055
+ * File      : Poligon.java
+ * Deskripsi : Representasi dasar dari objek poligon (segi banyak)
+ */
+
+package org.poligon;
+
+public class Poligon {
+    protected int jumlahSisi;
+
+    public int getJumlahSisi(){
+        return this.jumlahSisi;
+    }
+}
