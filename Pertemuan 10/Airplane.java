afd1b62c8a9075442aa98a87c44136bf0fc12f77
Asy'syifa Shabrina Munir <131370960+syifm@users.noreply.github.com>
2024-06-02 22:57:14 +0700

    Pertemuan 10 directory

--- Pertemuan 10/Airplane.java
+++
@@ -1,9 +0,0 @@
-/*
- * Nama      : Asy'syifa Shabrina Munir
- * NIM       : 24060122130055
- * File      : Airplane.java
- * Deskripsi : Kelas abstrak Airplane yang mewarisi kelas abstrak Vehicle dan interfacenya IFlyer
- */
-
-public abstract class Airplane extends Vehicle implements IFlyer{
-}
--- Pertemuan 10/Kryptonian.java
+++
@@ -1,9 +0,0 @@
-/*
- * Nama      : Asy'syifa Shabrina Munir
- * NIM       : 24060122130055
- * File      : Kryptonian.java
- * Deskripsi : Kelas abstrak Kryptonian yang mewarisi kelas Animal dan interfacenya yaitu IFlyer
- */
-
-public abstract class Kryptonian extends Animal implements IFlyer{
-}
